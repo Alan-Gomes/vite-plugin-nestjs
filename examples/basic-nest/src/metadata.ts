9c7a38883b0a57cb9a66676251610904f023771c
Alan Gomes <contato@alangomes.dev>
2026-05-09 17:14:14 -0300

    fix: lint issues

--- examples/basic-nest/src/metadata.ts
+++ examples/basic-nest/src/metadata.ts
@@ -1,5 +1,23 @@
 /* eslint-disable */
 export default async () => {
-    const t = {};
-    return { "@nestjs/swagger": { "models": [], "controllers": [[import("./app.controller"), { "AppController": { "getHello": { summary: "This is a test", description: "A greeting string", type: String } } }]] } };
+  const t = {};
+  return {
+    "@nestjs/swagger": {
+      models: [],
+      controllers: [
+        [
+          import("./app.controller"),
+          {
+            AppController: {
+              getHello: {
+                summary: "This is a test",
+                description: "A greeting string",
+                type: String,
+              },
+            },
+          },
+        ],
+      ],
+    },
+  };
 };
